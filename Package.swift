a8ccebe8b1361361fb42a1d29ddcd248225dee4c
Jacob <>
2021-04-25 12:47:29 +0100

    Update dependency on ThemeKit

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["ComponentKit"]),
     ],
     dependencies: [
-         .package(url: "https://github.com/jacobawhitehead/ThemeKit.git", from: "0.0.1"),
+         .package(url: "https://github.com/jacobawhitehead/ThemeKit.git", from: "0.0.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
